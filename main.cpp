65638b923023ff936f36fb243df5c25e0a4d772e
Emre <67150946+emreyzr@users.noreply.github.com>
2025-04-06 15:12:36 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file main.cpp
+ * @author 3017477 Emre Yazar
+ * @brief This is
+ * @version 1.0
+ * @date 2025-04-05
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+
+#include <iostream> //Importing necessary templates
+#include "Airplane.hpp" //Importing Airplane
+using namespace std;
+
+int main()
+{
+    Airplane A380;
+    A380.setYear(2007);
+    A380.setMaintenance(20241212);
+
+    std::cout<<A380.getYear()<<A380.getMaintenance()<<std::endl;
+    return 0;
+}
